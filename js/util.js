2d5d11d6c69081ef12c218fe7b21808be076ec04
mikeymop <midefran@gmail.com>
2019-12-16 23:10:01 -0500

    js shizz

---
+++ js/util.js
@@ -0,0 +1,13 @@
+export class Util {
+
+    songs = null;
+
+    constructor() {
+        this.songs = null;
+        this.create();
+    }
+
+    create() {
+        console.log("Util created");
+    }
+}
