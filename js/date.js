b632dfd594854bf5d0778e9311560576be95a1ae
mikeymop <midefran@gmail.com>
2019-12-17 01:14:43 -0500

    music widget is loading

--- js/date.js
+++ js/date.js
@@ -16,18 +16,4 @@ export class TimeStamp extends Date {
         return month;
 
     }
-
-    // getTimeStamp() {
-    //     let a = new Date(Number(this.unixtime));
-    //     let months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
-    //     let year = a.getFullYear();
-    //     let month = months[a.getMonth()];
-    //     let date = a.getDate();
-    //     let hour = a.getHours();
-    //     let min = a.getMinutes();
-    //     let sec = a.getSeconds();
-    //     let time = `${date} ${month} ${year}`;
-    //     //var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec ;
-    //     return time;
-    // }
 }
--- js/util.js
+++ js/util.js
@@ -36,16 +36,47 @@ export class Util {
     createMusicWidget(data) {
         data = JSON.parse(data);
         this.listens = data.payload.listens;
-        console.log(`creating music widget with:\n${this.listens}`);
+        console.log(`creating music widget...`);
         let t = new TimeStamp(1576541156 * 1000);
         console.log(`listen time is: ${t.getFullYear()}-${t.getMonthName()}`);
         let music_widget = document.querySelector('#music-widget');
         music_widget.innerHTML = `
           <h3>Recent Listens:</h3>
           <div id="disclaimer">Listen history courtesty of ListenBrainz</div>
-          <div id="track-list"
-            <div>
+          <div id="track-list">
+            <table class="table">
+              <thead>
+                <tr>
+                  <th>Listen Time:</th>
+                  <th>Artist:</th>
+                  <th>Song:</th>
+                </tr>
+              </thead>
+            </table>
           </div>
         `;
+        let table = document.querySelector('.table');
+        let tbody = document.createElement('tbody');
+        let i = 0;
+        for(let entry of this.listens) {
+            if(i == 10) { break; }
+            this.addRow(tbody, entry);
+            i += 1;
+        }
+        table.appendChild(tbody);
+    }
+
+    addRow(tbody, entry) {
+        console.log(`addrow`);
+        console.log(entry);
+        let t_stamp = new TimeStamp(Number(entry.listened_at));
+        let track = entry.track_metadata;
+        let row = document.createElement("tr");
+        row.innerHTML = `
+          <td>${t_stamp.getMonthName()} ${t_stamp.getDate()} ${t_stamp.getFullYear()}}</td>
+          <td>${track.artist_name}</td>
+          <td>${track.track_name}</td>
+        `;
+        tbody.appendChild(row);
     }
 }
