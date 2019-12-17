f83b203e4e63548b4440c18373aa692f87fa4e04
mikeymop <midefran@gmail.com>
2019-12-16 23:33:16 -0500

    loading js as an es6 module now

--- js/util.js
+++ js/util.js
@@ -1,6 +1,7 @@
 export class Util {
 
     songs = null;
+    widget = null;
 
     constructor() {
         this.songs = null;
@@ -9,5 +10,23 @@ export class Util {
 
     create() {
         console.log("Util created");
+    }
+
+    getSongHistory() {
+        let url = "https://api.listenbrainz.org/1/user/mikeymop/listens"
+        console.log(`Getting listen history from:\n${url}`);
+        fetch(url, {
+            "method": "GET",
+            "headers": "application/json"
+        }.then((response) => {
+            return response.json();
+        }).then((data) => {
+            console.log(`fetch got:\n${JSON.stringify(data)}`);
+            createMusicWidget(data);
+        });
+    }
+
+    createMusicWidget() {
+        console.log("creating music widget");
     }
 }
